73434b185a6cf26554984657e6e70ecc354e2f7c
Frederico Mota <frederico.mota@tjdft.jus.br>
2014-06-04 20:30:59 -0300

    correcao do bug que nao permitia reservar quantidade total de ingressos

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,15 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+	@Test
+	public void deveReservarQuantidadeTotalDisponivelDeIngressos() throws Exception {
+		
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(5);
+		boolean podeReservar=sessao.podeReservar(5);
+		
+		Assert.assertTrue(podeReservar);
+		
+	}
+	
 }
